5d63bbb0f133db6f21574e8565f78fd853b64954
fujiwara <fujiwara@fact-real.com>
2012-04-16 14:01:41 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+C2::Application.config.secret_token = 'a3649a8bc50a1cd9d3f01527b96bf2ce463ab6f58c447c13c972163d75d17f34ff55a67b78687023fc673b030775cca81b721c0530523dec9d8701c22261be19'
